708b1b4ea299c9ccc89986ccf784bb03c93948f7
FrancoisSuzeau <frsuzeau@gmail.com>
2020-10-07 23:24:44 +0200

    2020/10/07 - FrancoisTower - first unitary test made

--- src/com/uca/RomanConverter.java
+++ src/com/uca/RomanConverter.java
@@ -30,12 +30,61 @@ public class RomanConverter{
 
 	public static String getRomanFromNumber(int a) throws IllegalArgumentException{
 		//TODO
+		switch(a)
+		{
+			case 1 :
+				return "I";
+			case 2 :
+				return "II";
+			case 3 :
+				return "III";
+			case 4 :
+				return "IV";
+			case 5 :
+				return "V";
+			case 6 :
+				return "VI";
+			case 7 :
+				return "VII";
+			case 8:
+				return "VIII";
+			case 9:
+				return "IX";
+			case 10 :
+				return "X";
+			default:
+				throw new IllegalArgumentException("Roman number is not between 1 and 10");
+		}
 		
-		return "IV";
 	}
 	
 	public static int getNumberFromRoman(String a) throws IllegalArgumentException{
 		//TODO
-		return 0;
+
+		switch(a)
+		{
+			case "I" :
+				return 1;
+			case "II" :
+				return 2;
+			case "III" :
+				return 3;
+			case "IV" :
+				return 4;
+			case "V" :
+				return 5;
+			case "VI" :
+				return 6;
+			case "VII" :
+				return 7;
+			case "VIII":
+				return 8;
+			case "IX":
+				return 9;
+			case "X" :
+				return 10;
+			default:
+				throw new IllegalArgumentException("Roman number is not between 1 and 10");
+		}
 	}
 }
--- src/com/uca/Tests.java
+++ src/com/uca/Tests.java
@@ -5,15 +5,54 @@ import java.util.concurrent.Callable;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import java.util.ArrayList; //pour les premiers écrit de isAGoodRoman
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+
 public class Tests {
+
+    private List<String> RomanNumberList = Arrays.asList("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
 	
 	@Test
-	public void testConverter(){
+	/*public void testConverter(){
 		assertThat(RomanConverter.getRomanFromNumber(4), equalTo("IV"));
 		assertThat(exceptionOf(() -> RomanConverter.getRomanFromNumber(-2)), instanceOf(IllegalArgumentException.class));
-	}
+	}*/
 	
-	//TODO : les autres tests
+    public void allSuccessfullTest()
+    {
+        //isGoodDecimal();
+        isAGoodRoman();
+    }
+
+    public void isGoodDecimal()
+    {
+
+    }
+
+    public void isAGoodRoman()
+    {
+        
+        Iterator it = this.RomanNumberList.iterator();
+
+        int i = 1;
+        while (it.hasNext())
+        {
+            assertThat(RomanConverter.getNumberFromRoman((String)it.next()), equalTo(i));
+            i++;
+        }
+    }
+
+
+    /*public void allFailureTest()
+    {
+
+    }*/
+
+
+
+
 	
 
 
